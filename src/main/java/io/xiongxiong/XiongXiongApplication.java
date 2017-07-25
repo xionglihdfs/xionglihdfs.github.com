30f76f8f39604e2c3be7c90f2fab8d9847991985
xiongli <xionglihdfs@163.com>
2017-07-25 11:12:56 +0800

    初始化springboot项目.
    初始化springboot项目.

---
+++ src/main/java/io/xiongxiong/XiongXiongApplication.java
@@ -0,0 +1,20 @@
+package io.xiongxiong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+
+@SpringBootApplication
+public class XiongXiongApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiongXiongApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(XiongXiongApplication.class);
+	}
+}
---
+++ src/main/java/io/xiongxiong/modules/api/dao/TokenDao.java
@@ -0,0 +1,21 @@
+package io.xiongxiong.modules.api.dao;
+
+import io.xiongxiong.modules.api.entity.TokenEntity;
+import io.xiongxiong.modules.sys.dao.BaseDao;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 用户Token
+ * 
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2017-03-23 15:22:07
+ */
+@Mapper
+public interface TokenDao extends BaseDao<TokenEntity> {
+    
+    TokenEntity queryByUserId(Long userId);
+
+    TokenEntity queryByToken(String token);
+	
+}
